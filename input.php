d9facd55ebf404201ccae78f56ed16431ab3a5ab
Gadise Mustika <gadisee6@gmail.com>
2021-12-28 10:55:06 +0700

    add required in input form

--- input.php
+++ input.php
@@ -25,7 +25,7 @@
                     <label for="nama">
                         <span>Nama Tamu</span>
                     </label>
-                    <input type="text" name="nama" id="nama" placeholder="Tuliskan Nama Anda..">
+                    <input type="text" name="nama" id="nama" placeholder="Tuliskan Nama Anda.." required>
                 </div>
 
                 <div class="tipe-kamar">
@@ -33,7 +33,7 @@
                         <span>Tipe Kamar</span>
                     </label>
 
-                    <select name="tipe" id="tipe">
+                    <select name="tipe" id="tipe" required>
                         <option value="vip">VIP</option>
                         <option value="executive">Executive</option>
                         <option value="bisnis">Bisnis</option>
@@ -44,7 +44,7 @@
                     <label for="waktu">
                         <span>Lama Inap</span>
                     </label>
-                    <input type="text" name="waktu" id="waktu" placeholder="Mau Nginap Berapa Lama..">
+                    <input type="text" name="waktu" id="waktu" placeholder="Mau Nginap Berapa Lama.." required>
                 </div>
 
                 <div class="button">
